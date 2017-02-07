951eb1c251c9989616547fc6709cc0c25b213538
Enrique Salazar <enrique.salazar@gmail.com>
2017-02-07 14:32:41 +0100

    Webpack HMR setup

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+/**
+ * Created by enriq on 2/7/2017.
+ */
+import React from 'react';
+import styles from '../styles/App.css';
+
+const App = () => (
+    <div className={styles.app}>
+        <h2>React MusicPlayerrrrrr</h2>
+    </div>
+);
+
+export default App;
